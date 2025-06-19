e554bea1a52c9d1c6ecdb3219aab387e5c1d2b8e
Ezequiel-Elias <122619412+Ezequiel-Elias@users.noreply.github.com>
2025-06-19 10:21:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ document.getElementById("Botao").addEventListener("click", mostrarmensagem);
+
+ function mostrarmensagem() {
+    document.getElementById("mensagem").innerHTML = "Obrigado por corrigir meu trabalho!";
+  }
